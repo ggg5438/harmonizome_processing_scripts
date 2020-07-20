f506d52654239705d28ef65f29b95c4f35febb1d
ggg5438 <jngdngyp@naver.com>
2020-07-20 19:23:23 +0900

    Update download.py
    module 'urllib' has no attribute 'urlretrieve', and 'urllib.request' has.

--- wikipathways/main/download.py
+++ wikipathways/main/download.py
@@ -3,6 +3,6 @@ import urllib
 
 folder = 'input/'
 date = time.strftime('%Y%m%d', time.localtime())
-fileurl = 'http://www.pathvisio.org/data/bots/gmt/wikipathways.gmt'
+fileurl = 'http://www.pathvisio.org/data/bots/gmt/wikipathways.gmt' # 20/7/2020 now the server is closed.
 filename = 'dataset_{}_original.gmt'.format(date)
-urllib.urlretrieve(fileurl, folder + filename)
+urllib.request.urlretrieve(fileurl, folder + filename)
